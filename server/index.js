0361f598f1bc75c7afd98c845b407e9e435629ed
goswamivikas <vikas.mca17.du@gmail.com>
2022-02-09 18:32:48 +0530

    added auth and user routes

--- server/index.js
+++ server/index.js
@@ -3,6 +3,9 @@ const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 
+const authRoute = require("./routes/auth");
+const usersRoute = require("./routes/users");
+
 dotenv.config();
 
 mongoose
@@ -13,6 +16,11 @@ mongoose
   .then(() => console.log("db connection successfull!"))
   .catch((error) => console.log(error));
 
-app.listen(8800, () => {
+app.use(express.json());
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", usersRoute);
+
+app.listen(8888, () => {
   console.log("server is running");
 });
